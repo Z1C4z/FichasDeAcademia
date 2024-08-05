2cc80486a05f3ed95b5854d835ebc68c55224111
Z1C4z <andreluis5rabelo@gmail.com>
2024-08-05 17:22:02 -0300

    nova arquivo

--- screen_record.py
+++ screen_record.py
@@ -1,1 +1,17 @@
+from typing import Tuple
 import customtkinter as ctk
+
+class Screen_Record(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Edição de Fichas')
+        self.geometry('400x610')
+        self.screen()
+
+    def screen(self):
+        pass
+
+if __name__ == '__main__':
+    sys = Screen_Record()
+    sys.mainloop()
